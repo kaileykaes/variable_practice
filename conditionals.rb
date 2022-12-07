229628aa9727ac4dad580f2c10e73222cbd3850d
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2022-12-07 14:44:08 -0700

    Adjust string

---
+++ conditionals.rb
@@ -0,0 +1,18 @@
+is_open = true
+
+if is_open == true
+  puts "Let's go!"
+end
+
+
+
+age = 21
+if age == 21 
+    puts "Welcome to the Website"
+elsif age == 2 
+    puts "How did you even get here?"
+else 
+    puts "Give it a minute and come back."
+end 
+
+
